4f20cc87aa9349e7a709c1d80bd04aa4a8db1b36
elenazambrano <95417079+elenazambrano@users.noreply.github.com>
2022-05-31 17:39:52 +0200

    objectMother hecho

--- src/objectMother/SocioMother.java
+++ src/objectMother/SocioMother.java
@@ -1,5 +1,29 @@
 package objectMother;
 
+import java.util.ArrayList;
+
+import model.Socio;
+
 public class SocioMother {
 
+private ArrayList<Socio> misSocios = new ArrayList<Socio>();
+	
+	public boolean creaSocios(String matriculaEntrada) {
+		
+		//con el primer if controlo que la matricula no esté vacia
+		//si lo cumple retorna true y lo añade al arraylist, sino false
+		
+		if (matriculaEntrada != null) {
+			misSocios.add(new Socio(matriculaEntrada));
+			
+			return true;
+		}
+		return false;
+		
+	}
+
+	public ArrayList<Socio> getMisSocios() {
+		return misSocios;
+	}
+	
 }
